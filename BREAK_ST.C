4c099b6e486a3b6a3fb9092a5ffed8b0a3f9f82b
abhaysinghchauhan885 <abhaysinghchauhan885@gmail.com>
2025-05-21 10:56:56 +0530

    21-5-2025 updated

---
+++ BREAK_ST.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int i;
+	clrscr();
+	for(i=0;i<10;i++)
+	{
+		if(i==5)
+		{
+			continue;
+		}
+		else
+		{
+			printf("\nI : %d",i);
+		}
+	}
+	getch();
+}
